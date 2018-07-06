41564a67564fa8d77c5983666d5fd74b8e17012a
Triferoni <1347638091@qq.com>
2018-07-06 16:44:05 +0800

    Create help.cpp

---
+++ help.cpp
@@ -0,0 +1,23 @@
+#include "head.h"
+
+int main(){
+	help();
+	return 0;
+}
+void help(){
+	/*功能: 显示帮助命令*/
+	printf("command: \n\
+	help   ---  show help menu \n\
+	clear  ---  clear the screen \n\
+	cd     ---  change directory \n\
+	mkdir  ---  make directory   \n\
+	touch  ---  create a new file \n\
+	cat    ---  open and read a exist file \n\
+	vi     ---  open and write something to a file \n\
+	close  ---  close a file \n\
+	rm     ---  delete a exist file \n\
+	rm -r  ---  delete a directory \n\
+	format ---  format a exist filesystem \n\
+	sudo   ---  switch user \n\
+	exit   ---  exit this system \n");
+}
