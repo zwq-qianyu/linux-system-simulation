9d2b31ded602af7e889d9f12396d425e94a01037
Triferoni <1347638091@qq.com>
2018-07-08 15:48:14 +0800

    融合时和鲁恒统一变量所做的修改
    统一的变量：
    int argc;		// 用户命令的参数个数
    char *argv[5];		// 用户命令的参数
    
    改为
    
    vector<string>vc_of_str;    // 用户命令的参数个数
    string  s1, s2;            // 用户命令的参数

--- close.cpp
+++ close.cpp
@@ -1,12 +1,12 @@
 #include "head.h"
 File_table file_array[FILENUM];	// 打开文件表数组
 
-void close(char *argv1){
+void close(string argv1){
 	/*功能: 关闭已经打开的文件(close file1)*/
 	int i;
 	for (i = 0; i < FILENUM; i++)
 		if ((file_array[i].inum > 0) &&
-			!strcmp(file_array[i].file_name, argv1)) break;
+			(argv1 == file_array[i].file_name)) break;
 	if (i == FILENUM){
 		printf("This file didn't be opened.\n");
 		return;
