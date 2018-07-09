bd04e068f472bc9bb23265085c83d4aefc330c42
Triferoni <1347638091@qq.com>
2018-07-09 12:20:50 +0800

    加了一个判断条件，判断是否在同一目录。使得在不同目录下同名文件的vi操作不会出现bug

--- open.cpp
+++ open.cpp
@@ -1,27 +1,25 @@
-#include "head.h"
-Inode inode_array[INODENUM];	// i节点数组
-File_table file_array[FILENUM];	// 打开文件表数组
-
-void open(int mymode, string argv1){
+void open(int mymode, string argv1) {
 	/*功能: 打开当前目录下的文件(open file1)*/
 	int i, inum, mode, filenum, chk;
 	for (i = 0; i < INODENUM; i++)
 		if ((inode_array[i].inum > 0) &&
 			(inode_array[i].type == '-') &&
-			(argv1 == file_array[i].file_name))
+			argv1 == inode_array[i].file_name &&
+			!strcmp(inode_array[i].user_name, user.user_name) &&
+			inode_array[i].iparent == inum_cur)     //判断是否在当前目录下 
 			break;
-	if (i == INODENUM){
-		cout<<"This is no " + argv1 + " file...\n";
+	if (i == INODENUM) {
+		cout << "This is no " + argv1 + " file...\n";
 		return;
 	}
 	inum = i;
 	chk = check(i);               //检查该文件是否为当前用户的文件
-	mode = mymode; 
-	if (chk != 1){
+	mode = mymode;
+	if (chk != 1) {
 		printf("This file is not yours !\n");
 		return;
 	}
-	if ((mode < 1) || (mode > 3)){
+	if ((mode < 1) || (mode > 3)) {
 		printf("Open mode is wrong.\n");
 		return;
 	}
