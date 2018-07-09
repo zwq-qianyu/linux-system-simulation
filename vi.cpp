850904bb5f912272216ab4ec1d7ee3016b1dfedb
Triferoni <1347638091@qq.com>
2018-07-09 10:15:31 +0800

    用户输入预分配大小，但是还未实现多行输入

--- vi.cpp
+++ vi.cpp
@@ -1,16 +1,7 @@
-#include "head.h"
-//int argc;		// 用户命令的参数个数
-//char *argv[5];		// 用户命令的参数
-Inode inode_array[INODENUM];	// i节点数组
-File_table file_array[FILENUM];	// 打开文件表数组
-char temp[2 * BLKSIZE];	// 缓冲区
-
-vector<string>vc_of_str;    // 用户命令的参数个数
-string  s1, s2;            // 用户命令的参数
-
-void vi(){
+void vi() {
 	/*功能: 向文件中写入字符(write file1)*/
 	int i, inum, length;
+	//length = 0;
 	/*if (argc != 2){
 		printf("Command write must have one args. \n");
 		return;
@@ -18,33 +9,50 @@ void vi(){
 	open(3, s2);
 	for (i = 0; i < FILENUM; i++)
 		if ((file_array[i].inum>0) &&
-			(s2 == file_array[i].file_name)) break;
+			s2==file_array[i].file_name) break;
+	if (i == FILENUM){
+		cout << "Open " << s2 << " false.\n";
+		return;
+	}
 	inum = file_array[i].inum;
 	//printf("The length of %s:%d\n", inode_array[inum].file_name, inode_array[inum].length);
 	if (inode_array[inum].length == 0){
-		inode_array[inum].length = 1024;
-		inode_array[inum].address[0] = get_blknum();    //一次分配只有512B的空间，所以调用2次get_blknum()获取1024B空间
-		inode_array[inum].address[1] = get_blknum();
+		printf("The length you want to write(0-1024):");
+		scanf("%d", &length);
+		gets(temp);
+		if ((length < 0) && (length >1024)){
+			printf("You can't creat a file which data less than 0 byte or more than 1024 bytes.'.\n");
+			return;
+		}
+		inode_array[inum].length = length;
+		inode_array[inum].address[0] = get_blknum();
+		if (length > 512)
+			inode_array[inum].address[1] = get_blknum();
 		save_inode(inum);
-		/*printf("Input the data(Enter to end):\n");
-		gets(temp);*/
-		printf("Input the data(End by #):\n");
-		char c;
-		char *m;
-		string str;
-		while((c=getchar())!='#'){
-			if (c == 0x0d){ //当输入回车键时，0x0d为回车键的ASCII码
-				c = '\0'; //将输入的回车键转换成空格
-			}
-			str = str + gets(m) +"\n";	
-		}
-		for(int i=0;i<1024;i++){
-			temp[i] = str[i];
-		}
-		//printf(temp);
+		printf("Input the data(Enter to end):\n");
+		gets(temp);
 		write_blk(inum);
 	}
-	else
-		printf("This file has content,it can't be written.\n");
+	else{
+		printf("Are you sure to write this file? Data in it will all be deleted.(y/n):");
+		scanf("%c", &choice);
+		if ((choice == 'y') || (choice == 'Y')){
+			printf("The length you want to write(0-1024):");
+			scanf("%d", &length);
+			gets(temp);
+			if ((length < 0) && (length >1024)){
+				printf("You can't creat a file which data less than 0 byte or more than 1024 bytes.'.\n");
+				return;
+			}
+			inode_array[inum].length = length;
+			inode_array[inum].address[0] = get_blknum();
+			if (length > 512)
+				inode_array[inum].address[1] = get_blknum();
+			save_inode(inum);
+			printf("Input the data(Enter to end):\n");
+			gets(temp);
+			write_blk(inum);
+		}
+	}
 	close(s2);
 }
